35fd14cac34ddf08ad63fd807eeb183627b923b0
Bernardo Lucas de Sousa <bernardosaopaulino@gmail.com>
2022-11-20 21:07:30 -0300

    Exercicio C#

---
+++ projeto2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DIGITE UM NUMERO:");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());  
+            if (a % b == 0) {
+                Console.WriteLine("SAO MULTIPLOS"); 
+
+            }
+            else if (b % a == 0)
+            {
+                Console.WriteLine("SAO MULTIPLOS");
+            }
+            else
+            {
+                Console.WriteLine("NAO SAO MULTIPLOS");
+            }
+
+        }
+    }
+}
